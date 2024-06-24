1fca3bbddd656c3bcffd1a0e15d0ef19cb10faa8
jeetkaur1707 <167002766+jeetkaur1707@users.noreply.github.com>
2024-06-24 23:11:31 +0530

    upadat request api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 # Create a Flask application instance
 app = Flask(__name__)
@@ -6,7 +7,23 @@ app = Flask(__name__)
 # Define a route for the root URL (/) - this is the default route
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    response = requests.get('https://status.skyserver.in/status.php')
+    data = response.json()
+    status = data['serverStatus']
+    days = data['uptime']['days']
+    hours = data['uptime']['hours']
+    mins = data['uptime']['mins']
+    secs = data['uptime']['secs']
+    uptime_percentage = data['uptime_percentage']
+    load = data['load']
+
+    response_aapanal = requests.get('http://thelocalstreet.in/hardik/uptime.json')
+    aapanal_data = response_aapanal.text
+
+    # server_status= aapanal_data['server_status']
+    # print(server_status)
+
+    return render_template('index.html',data=status,days=days,hours=hours,mins=mins,secs=secs,uptime_percentage=uptime_percentage,load=load)
 
 # Run the Flask application
 if __name__ == '__main__':
